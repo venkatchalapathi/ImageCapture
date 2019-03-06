274983ff15093c3b880fa88037dd9da149e6581e
Venkata Chalapathi <venkat.pv@gmail.com>
2019-03-06 11:00:54 +0530

    Initial commit

---
+++ app/src/main/java/com/example/imagecapture/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.imagecapture;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+    //TO DO's
+   /* If an essential function of your application is taking pictures, then restrict its visibility on Google Play to devices
+    that have a camera. To advertise that your application depends on having a camera, put a <uses-feature> tag in your manifest file
+
+    <manifest ... >
+    <uses-feature android:name="android.hardware.camera"
+                  android:required="true" />
+    ...
+    </manifest>
+
+    */
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+    }
+
+    public void startCam(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+ }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
+}
